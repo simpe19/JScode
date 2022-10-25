e36c47bfe3411010befbb34d9c2d846271754287
Simon Gummesson <Simp19@live.se>
2022-10-25 13:24:49 +0200

    klart

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const MenuIcon = ({link, icon, quantity}) => {
+const MenuIcon = ({link, icon, quantity, hideMobile}) => {
   return (
-    <NavLink className={"menu-icon"}  to={link} end>
+    <NavLink className={`menu-icon ${hideMobile ? "hidden" : ""}`} to={link} end>
       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
         <i className={icon}></i>
       </NavLink>
--- src/sections/MainMenuSection.js
+++ src/sections/MainMenuSection.js
@@ -7,7 +7,7 @@ const MainMenuSection = () => {
     <nav className="head">
       <div className='color container'>
             <NavLink className="logo" to='/' end>Fixxo.</NavLink>
-        <div className="navigation">
+        <div className="menu-links">
             <NavLink className="menu-link"  to='/' end>Home</NavLink>
             <NavLink className="menu-link"  to='/categories' end>Categories</NavLink>
             <NavLink className="menu-link"  to='/products' end>Products</NavLink>
@@ -15,8 +15,8 @@ const MainMenuSection = () => {
         </div>
         <div className='menu-icons'>
             <MenuIcon link="/search" icon="fa-solid fa-magnifying-glass" />
-            <MenuIcon link="/compare" icon="fa-regular fa-code-compare fa-flip-horizontal" />
-            <MenuIcon quantity='3' link="/wishlist" icon="fa-regular fa-heart" />
+            <MenuIcon hideMobile='true' link="/compare" icon="fa-regular fa-code-compare fa-flip-horizontal" />
+            <MenuIcon hideMobile='true' quantity='3' link="/wishlist" icon="fa-regular fa-heart" />
             <MenuIcon quantity='4' link="/shoppingcart" icon="fa-regular fa-bag-shopping" />
         </div>
       </div>
