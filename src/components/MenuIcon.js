e05e3a080d03c0864e58630d90989671b7b395f0
Simon Gummesson <Simp19@live.se>
2022-10-26 14:05:32 +0200

    mainmenu

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const MenuIcon = ({link, icon, quantity, hideMobile}) => {
   return (
-    <NavLink className={`menu-icon ${hideMobile ? "hidden" : ""}`} to={link} end>
+    <NavLink className={ `menu-icon ${hideMobile ? 'd-none d-md-flex' : ''}` } to={link} end>
       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
         <i className={icon}></i>
       </NavLink>
--- src/sections/MainMenuSection.js
+++ src/sections/MainMenuSection.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import MenuIcon from '../components/MenuIcon'
 
 const MainMenuSection = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
   return (
     <nav className="head">
       <div className='color container'>
             <NavLink className="logo" to='/' end>Fixxo.</NavLink>
-        <div className="menu-links">
+        <div className={`menu-links ${ showMenu ? 'd-grid': ''}`}>
             <NavLink className="menu-link"  to='/' end>Home</NavLink>
             <NavLink className="menu-link"  to='/categories' end>Categories</NavLink>
             <NavLink className="menu-link"  to='/products' end>Products</NavLink>
@@ -15,9 +20,11 @@ const MainMenuSection = () => {
         </div>
         <div className='menu-icons'>
             <MenuIcon link="/search" icon="fa-solid fa-magnifying-glass" />
-            <MenuIcon hideMobile='true' link="/compare" icon="fa-regular fa-code-compare fa-flip-horizontal" />
-            <MenuIcon hideMobile='true' quantity='3' link="/wishlist" icon="fa-regular fa-heart" />
+            <MenuIcon hideMobile={true} link="/compare" icon="fa-regular fa-code-compare fa-flip-horizontal" />
+            <MenuIcon hideMobile={true} quantity='3' link="/wishlist" icon="fa-regular fa-heart" />
             <MenuIcon quantity='4' link="/shoppingcart" icon="fa-regular fa-bag-shopping" />
+
+            <button onClick={toggleMenu} className='d-xl-none menu-icon btn-menu-icon'><i className="fa-regular fa-bars"></i></button>
         </div>
       </div>
     </nav>
