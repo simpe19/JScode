8ecbe647a081f23b59ea6ed28f4b0f9a4b45a872
Simon Gummesson <Simp19@live.se>
2022-11-07 12:58:09 +0100

    res

--- src/sections/ContactFormSection.js
+++ src/sections/ContactFormSection.js
@@ -1,89 +1,116 @@
 import React, { useState } from 'react'
+import { validate } from '../scripts/validation'
 
-const validate = (event) => {
-    switch(event.type) {
-        case 'keyup':
-            validateElement(event.target)
-            break;
-    }
-}
-const validateElement = (event) => {
-    
-}
+const ContactForm = () => {
+  let currentPage = "Contact Us"
+  window.top.document.title = `${currentPage} || Fixxo` 
 
-const ContactFormSection = () => {
-    const [contactForm, setContactForm] = useState({name: '', email: '', comment: ''})
-    const [formErrors, setFormErrors] = useState({})
-    const [canSubmit, setCanSubmit] = useState(false)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [comments, setComments] = useState('')
+  const [errors, setErrors] = useState({})
+  const [submitted, setSubmitted] = useState(false)
+  const [failedSubmit, setFailedSubmit] = useState(false)
 
-    const validate = (values) => {
-        const errors = {}
-        const regex_email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const handleChange = (e) => {
+    const {id, value} = e.target
 
-        if(!values.name)
-            errors.name = "You must enter a name"
-
-        if(!values.email)
-            errors.email = "You must enter an email"
-        else if(!regex_email.test(values.email))
-            errors.email = "You must enter an valid email (eg. name@domain.com)"
-
-        if(!values.comment)
-            errors.comment = "You must enter a comment"
-        else if(values.comment.length < 5)
-            errors.comment = "You must enter a minimum of five characters"
-
-        if(Object.keys(errors).length === 0)
-            setCanSubmit(true)
-        else
-            setCanSubmit(false)
-
-        return errors;
+    switch(id) {
+      case 'name':
+        setName(value)
+        break
+      case 'email':
+        setEmail(value)
+        break
+      case 'comments':
+        setComments(value)
+        break
     }
 
-    const handleChange = (e) => {
-        const {id, value} = e.target
-        setContactForm({...contactForm, [id]:value})
+    setErrors({...errors, [id]: validate(e)})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFailedSubmit(false)
+    setSubmitted(false)
+    setErrors(validate(e, {name, email, comments}))
+  
+    if (errors.name === null && errors.email === null && errors.comments === null) {
+        
+        let json = JSON.stringify({ name, email, comments})
+        
+        setName('')
+        setEmail('')
+        setComments('')
+        setErrors({})
+        
+        fetch('https://win22-webapi.azurewebsites.net/api/contactform', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+            body: json
+        })
+        .then(res => {
+
+            if (res.status === 200){
+                setSubmitted(true)
+                setFailedSubmit(false)
+            }
+            else {
+                setSubmitted(false)
+                setFailedSubmit(true)
+            }
+        })
+
+    } else {
+      setSubmitted(false)
     }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setFormErrors(validate(contactForm))
-    }
+  }
+
 
   return (
-    <section className='contact-form'>
-        <div className='container'>
-            {
-                canSubmit ? (<div className='d-flex justify-content-center align-items-center'>
-                    <div>Thank you for your comment</div>
-                </div>)
-                :
-                (
-                    <>
-            <h2>Come in contact with us</h2>
-            <form onSubmit={handleSubmit} noValidate>
-                <div>
-                    <input id='name' type='text' placeholder='Your Name' onkeyup="validate(event)" value={contactForm.name} onChange={handleChange}/>
-                    <div className='errorMessage'>{formErrors.name}</div>
-                </div>
-                <div>
-                    <input id='email' type='email' placeholder='Your Mail' onkeyup="validate(event)" value={contactForm.email} onChange={handleChange}/>
-                    <div className='errorMessage'>{formErrors.email}</div>
-                </div>
-                <div className='textarea'>
-                    <textarea id='comment' placeholder='comments' onkeyup="validate(event)" value={contactForm.comment} onChange={handleChange}></textarea>
-                    <div className='errorMessage'>{formErrors.comment}</div>
-                </div>
-                <div className='form-btn'>
-                    <button type='submit' className='_btn-theme'>Post Comment</button>
-                </div>
-            </form>
-                    </>
-                )
-            }
-        </div>
+    <section className="contact-form mt-5">
+      <div className="container">
+        
+        {
+          submitted ? (
+          <div className="alert alert-success text-center mb-5" role="alert">
+            <h3>Thank you for your comments</h3> 
+            <p>We will contact you as soon as possible.</p>
+            </div>  ) : (<></>)
+        }
+        {
+          failedSubmit ? (
+          <div className="alert alert-warning text-center mb-5" role="alert">
+            <h3>Something went wrong!</h3> 
+            <p>We couldn't submit your comment right now.</p>
+            </div>  ) : (<></>)
+        }
+        
+        
+        <h2>Come in Contact with Us</h2>
+        <form onSubmit={handleSubmit} noValidate>
+          <div>
+            <input id="name" className={(errors.name ? 'error': '')} value={name} onChange={handleChange} type="text" placeholder="Your Name" />
+            <div className="errorMessage">{errors.name}</div>
+          </div>
+          <div>
+            <input id="email" className={(errors.email ? 'error': '')} value={email} onChange={handleChange} type="email" placeholder="Your Mail" />
+            <div className="errorMessage">{errors.email}</div>
+          </div>
+          <div className="textarea">
+            <textarea id="comments" className={(errors.comments ? 'error': '')} style={(errors.comments ? {border: '1px solid #FF7373'}: {} )} value={comments} onChange={handleChange} placeholder="Comments"></textarea>
+            <div className="errorMessage">{errors.comments}</div>
+          </div>
+          <div className="form-btn">
+            <button type="submit" className="_btn-theme">Post Comments</button>
+          </div>
+        </form>    
+      </div>
     </section>
   )
 }
 
-export default ContactFormSection
+export default ContactForm
